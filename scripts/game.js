cbfcc266d89c51cedf1939ff2140f63028857eab
Boyan Todorov <boyan.digital@gmail.com>
2020-01-06 23:15:54 +0200

    JS

--- scripts/game.js
+++ scripts/game.js
@@ -3,7 +3,7 @@ sessionStorage.setItem('data', 'It\'s Alive')
 let a = sessionStorage.getItem('data')
 
 function inc() {
-    sessionStorage.setItem('data', 'It\'s Aliveeee ...")
+    sessionStorage.setItem('data', 'It\'s Aliveeee ...')
     a = sessionStorage.getItem('data')
     console.log(a)
 }
